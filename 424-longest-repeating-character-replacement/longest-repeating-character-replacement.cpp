a7bcfc490a32e8d6b16268a3b1eec0156ca48889
Pushpesh Pant <pushpeshpant023@gmail.com>
2026-07-29 17:32:58 +0530

    Time: 19 ms (12.97%) | Memory: 11 MB (42.15%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int>mp;
+        int freq=0;
+        int longest=0;
+        int j=0;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+            freq=max(freq,mp[s[i]]);
+            if((i-j+1)-freq>k){
+                mp[s[j]]--;
+                j++;
+            }
+            longest=max(longest,i-j+1);
+        }
+    return longest;
+    }
+};

c10a86a4bd73e4764f22321293e6749ee4ce3b12
Pushpesh Pant <pushpeshpant023@gmail.com>
2026-08-02 17:19:36 +0530

    Time: 3 ms (18.72%) | Memory: 9.3 MB (25.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-      int i=0;
-      int j=0;
-      while(i<haystack.size() && j<needle.size()){
-        if(haystack[i]==needle[j]){
-            i++;
-            j++;
+        vector<int>lps(needle.size(),0);
+            int len=0;
+            int i=1;
+            while(i<needle.size()){
+                if(needle[i]==needle[len]){
+                    len++;
+                    lps[i]=len;
+                    i++;
+                }
+                else{
+                    if(len!=0){
+                        len=lps[len-1];
+                    }
+                    else{
+                        lps[i]=0;
+                        i++;
+                    }
+                }
+            }
+            if(needle.empty()) return 0;
+            i=0;
+            int j=0;
+            while(i<haystack.size()){
+                if(haystack[i]==needle[j]){
+                    i++;
+                    j++;
+                }
+                if(j==needle.size()) {
+                    return i-j;
+            }
+            else if(i<haystack.size() && haystack[i]!=needle[j]){
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
         }
-      else{
-        i=i-j+1;
-        j=0;
-      }
-      if(j==needle.size()){
-        return i-j;
-      }
-      }
-     return -1;
+        return -1;
     }
 };
